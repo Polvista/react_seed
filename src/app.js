60bd0ce6b5dc0d86a7ed6bfbb6603911f00c9937
Alexey Sokolov <sokol789@yandex.ru>
2016-01-20 17:55:22 +0300

    h w

---
+++ src/app.js
@@ -0,0 +1,12 @@
+class App extends React.Component {
+    render() {
+        return (
+            <div>lol</div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
