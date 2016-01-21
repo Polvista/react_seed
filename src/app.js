d291c91e19c4b8851cbc39e6d4de917f2d796059
Alexey Sokolov <sokol789@yandex.ru>
2016-01-21 14:06:10 +0300

    add redux

--- src/app.js
+++ src/app.js
@@ -1,12 +1,42 @@
+const { createStore } = Redux;
+const { Provider, connect } = ReactRedux;
+
+let appReducer = (state = 0, action) => {
+    switch (action.type) {
+        case 'ADD':
+            return state + 1;
+        default:
+            return state;
+    }
+};
+
+let AppStore = createStore(appReducer);
+
+let AppActions = {
+    add(){
+        AppStore.dispatch({type:'ADD'});
+    }
+};
+
 class App extends React.Component {
+    divClicked(){
+        AppActions.add();
+    }
+
     render() {
+        let {number} = this.props;
+
         return (
-            <div>lol</div>
+            <div onClick={this.divClicked}>{number} lol</div>
         );
     }
 }
 
+App = connect(state => ({number: state}))(App);
+
 ReactDOM.render(
-    <App />,
+    <Provider store={AppStore}>
+        <App />
+    </Provider>,
     document.getElementById('root')
 );
