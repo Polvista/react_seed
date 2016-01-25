745683fb2400cf3d0d02cf67016c2102e922e359
Alexey Sokolov <sokol789@yandex.ru>
2016-01-25 18:10:46 +0300

    add some styles

--- src/counter/Counter.js
+++ src/counter/Counter.js
@@ -13,10 +13,10 @@ class Counter extends React.Component {
             <div className="counter">
                 <h4>Hey</h4>
                 <p className="lead">Counter: {number}</p>
-                <button onClick={counterActions.add} className="btn btn-default">lol</button>
-                <button onClick={counterActions.remove} className="btn btn-default">no lol</button>
-                <button onClick={counterActions.reset} className="btn btn-default">reset</button>
-                <button onClick={() => counterActions.setValue(20)} className="btn btn-default">set 20</button>
+                <a onClick={counterActions.add} className="btn btn-default">lol</a>
+                <a onClick={counterActions.remove} className="btn btn-default">no lol</a>
+                <a onClick={counterActions.reset} className="btn btn-default">reset</a>
+                <a onClick={() => counterActions.setValue(20)} className="btn btn-default">set 20</a>
             </div>
         );
     }
